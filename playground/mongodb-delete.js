702387f5d07a699b1e191e80f9fe5dd15c66eea3
ChangeME <you@example.com>
2018-01-04 11:09:31 -0500

    Delete some documents

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -9,17 +9,25 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, database) => {
     const db = database.db("TodoApp");
     
     // deleteMany
-    db.collection("Todos").deleteMany({text: "Eat lunch"}).then((result) =>{
-        console.log(result);
-    });
+    // db.collection("Todos").deleteMany({text: "Eat lunch"}).then((result) =>{
+    //     console.log(result);
+    // });
     // deleteOne
-    db.collection("Todos").deleteOne({text: "Eat lunch"}).then((result) =>{
-        console.log(result);
-    });
+    // db.collection("Todos").deleteOne({text: "Eat lunch"}).then((result) =>{
+    //     console.log(result);
+    // });
     // findOneAndDelete
-
+    // db.collection("Todos").findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
 
     
+    // db.collection("Users").deleteMany({name: "Max"}).then((result) =>{
+    //     console.log(result);
+    // });
+    // db.collection("Users").findOneAndDelete({_id: new ObjectID("5a4bae2615a1a05e008bcea1")}).then((result) => {
+    //     console.log(result);
+    // });
 
     // database.close();
 });
