4514b632b370e54293c475ead312bf7eb488b794
ChangeME <you@example.com>
2018-01-06 11:10:30 -0500

    Add test suites for POST /todos

---
+++ server/tests/server.test.js
@@ -0,0 +1,60 @@
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const request = require("supertest");
+
+const {app} = require("./../server");
+const {Todo} = require("./../models/todo");
+
+
+beforeEach((done) => {
+    Todo.remove({}).then((req, res) => {
+        done();
+    });
+});
+
+describe("POST /todos", () => {
+    it("should create a new todo", (done) => {
+        var text = "Test todo test";
+
+        request(app)
+        .post("/todos")
+        .send({text})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.text).to.equal(text);
+        })
+        .end((err, res) => {
+            if (err){
+                return done(err);
+            }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).to.equal(1);
+                expect(todos[0].text).to.equal(text);
+                done();
+            }).catch((e) => {
+                done(e);
+            });
+        });
+    });
+
+    it("should not create todo with invalid body data", (done) => {
+        request(app)
+        .post("/todos")
+        .send({text: ""})
+        .expect(400)
+        .end((err, res) => {
+            if (err){
+                return done(err);
+            }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).to.equal(0);
+                done();
+            }).catch((e) => {
+                done(e);
+            });
+        });
+    });
+});
+
