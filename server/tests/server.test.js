a130be5560db20d98fa7d4930430c1a807f69bdd
ChangeME <you@example.com>
2018-01-06 17:01:24 -0500

    Add test cases for DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -105,3 +105,40 @@ describe("GET /todos/:id", () => {
         .end(done);
     });
 });
+
+describe("DELETE /todos/:id", () => {
+    it("should remove a todo", (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).to.equal(hexId);
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).to.be.null;
+                done();
+            }).catch((e) => {
+                done(e);
+            });
+        });
+    });
+    it("should return 404 if todo not found", (done) => {
+        request(app)
+        .delete(`/todos/${(new ObjectID).toHexString()}`)
+        .expect(404)
+        .end(done); 
+    });
+    it("should return 404 if object id not valid", (done) => {
+        request(app)
+        .delete(`/todos/1234`)
+        .expect(404)
+        .end(done);
+    });
+});
