4f258f143f1cbf80e952ddbfe389b4300aa596ec
ChangeME <you@example.com>
2018-01-22 08:34:43 -0500

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require("mongodb");
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
+var {authnticate} = require("./middleware/authenticate");
 
 const port = process.env.PORT || 3000;
 var app = express();
@@ -108,6 +109,12 @@ app.post("/users", (req, res) => {
         res.status(400).send(e);
     });
 
+});
+
+
+
+app.get("/users/me", authnticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
